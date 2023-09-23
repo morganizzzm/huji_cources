60bb33b8e1b9491e59f219c292135f212ef4b074
morganizzzm <89296464+morganizzzm@users.noreply.github.com>
2023-09-23 13:41:02 +0300

    Add files via upload

---
+++ image-processing/ex5/README.cmd
@@ -0,0 +1,31 @@
+morganizm
+List of Files:
+- IP-EX5.pdf -- my PDF
+- IMPR_Ex5_Deep_Style_Image_Prior_2022_2023_morganizm.ipynb -- The code
+- Inpainting Folder:
+   - Fei-Fei-Li-EXAMPLE -- Fei-Fei-Li example files
+     - final_inverted_image_fei_inpainting.png
+     - inverted_latent_fei_inpainting.npz
+   - My-EXAMPLE -- My inpainting example files
+     - final_inverted_image_my_inpainting.png
+     - inverted_latent_my_inpainting.npz
+     - original_degraded_image_fei_inpainting.png
+     - original_degraded_image_my_inpainting.png
+- Deblurring
+   - Yann-LeCun-EXAMPLE -- Yann-LeCun example files
+     - inverted_latent-yann_deblur.npz
+     - final_inverted_image-yann_deblur.png
+   - My-EXAMPLE - My deblurring example files
+     - final_inverted_image_my_deblur.png
+     - inverted_latent-my_deblur.npz
+     - original_degraded_image_my_deblur.png
+     - original_not_degraded_image_my_deblur.jpeg
+- Colorization
+   - Alan-Turing-EXAMPLE -- Yann-LeCun example files
+     - final_inverted_image-alan_colorization.png
+     - inverted_latent-alan_colorization.npz
+   - My-EXAMPLE - My colorization example files
+     - original_notdegraded_image_my_colorization.jpeg
+     - inverted_latent_my_colorization.npz
+     - original_degraded_image_my_colorization.png
+     - final_inverted_image_my_colorization.png
